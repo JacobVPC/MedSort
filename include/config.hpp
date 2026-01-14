a68110803dee4475c1a191ddfad77c1894ab289e
JacobVPC <Jacobvpc@gmail.com>
2026-01-14 10:18:34 -0500

    finish patient inheritcance system

--- include/config.hpp
+++ include/config.hpp
@@ -1,8 +1,11 @@
 #include <string>
 
+
+
+
 class Patient{
 
-private:
+protected:
 int age;
 int priority;
 std::string name;
@@ -16,40 +19,42 @@ Patient(int a, int p, std::string n) : age(a) , priority(p) , name(n){}
 
 
 
-class ShortTerm_Patient{
+class ShortTerm_Patient : public Patient{
 
 private:
 
-int age;
-std::string name;
 std::string timeAssessed;
 int time_needed;
 std::string perscription;
 
 public:
 //constructor
-ShortTerm_Patient(int a, std::string n, std::string ta, int tn, std::string per) 
-: age(a) , name(n), timeAssessed(ta), time_needed(tn), perscription(per){}
+ShortTerm_Patient(int a, std::string n, int p, std::string ta, int tn, std::string per) 
+: Patient(a,p,n),
+timeAssessed(ta),
+time_needed(tn),
+perscription(per){}
 
 };
 
 
 
 
-class LongTerm_Patient{
+class LongTerm_Patient : public Patient{
     
 private:
 
-int age;
-std::string name;
 std::string timeAssessed;
 int time_needed;
 std::string perscription;
 
 public:
 //constructor
-LongTerm_Patient(int a, std::string n, std::string ta, int tn, std::string per) 
-: age(a) , name(n), timeAssessed(ta), time_needed(tn), perscription(per){}
+LongTerm_Patient(int a,int p, std::string n, std::string ta, int tn, std::string per) 
+: Patient(a,p,n),
+timeAssessed(ta),
+time_needed(tn), 
+perscription(per){}
 
 };
 
